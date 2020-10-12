6c634477816b6a174b34028f3c3e7a9b0afeac0a
Kartikeya Shorya <kartikeyashorya@gmail.com>
2020-10-13 01:13:15 +0530

    Implemented Generator

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+from utils import ResidualBlock
 
 
 class PatchGAN(nn.Module):
@@ -25,3 +27,41 @@ class PatchGAN(nn.Module):
     def forward(self, x1, x2):
         x = torch.cat([x1, x2], 1)
         return self.model(x)
+
+
+class Generator(nn.Module):
+    def __init__(self, num_res_layers=9):
+        super(Generator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            nn.InstanceNorm2d(256),
+            nn.ReLU(),
+            *self._make_res_layers(num_res_layers),
+            nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1,
+                output_padding=1),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1,
+                output_padding=1),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 3, kernel_size=7, stride=1, padding=3),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+    def _make_res_layers(self, num_layers):
+        layers = []
+        for _ in range(num_layers):
+            layers.append(ResidualBlock(256))
+
+        return layers
